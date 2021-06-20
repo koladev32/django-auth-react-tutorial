bd41e356d5cd33ca12fe14648d0d7332e537edce
koladev <onaelmangabo@gmail.com>
2021-06-20 13:26:04 +0100

    - fix: fix typo

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,8 +1,8 @@
 from core.user.models import User
-from rest_framework import serializer
+from rest_framework import serializers
 
 
-class UserSerializer(serializer.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
