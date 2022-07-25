02eac4ff918b5e240b167b08468a133446f3b479
koladev <onaelmangabo@gmail.com>
2022-07-25 10:17:15 +0100

    :recycle: minor refactoring

--- core/auth/apps.py
+++ core/auth/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class AuthConfig(AppConfig):
-    name = 'auth'
+    name = 'core.auth'
+    label = 'core_auth'
--- core/auth/viewsets/login.py
+++ core/auth/viewsets/login.py
@@ -1,19 +1,19 @@
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from core.auth.serializers import LoginSerializer
 
 
-class LoginViewSet(ModelViewSet, TokenObtainPairView):
+class LoginViewSet(ViewSet):
     serializer_class = LoginSerializer
     permission_classes = (AllowAny,)
     http_method_names = ['post']
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
         try:
             serializer.is_valid(raise_exception=True)
